908d9037b70c446b8f55397e7b30e33dea3124fe
Alucard2169 <alucardd2169@gmail.com>
2024-07-26 12:39:56 +0530

    implemented scroll in chat list component

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,10 +1,11 @@
 import userpfp from "../assets/userpfp.png"
 import searchIcon from "../assets/icons/searchIcon.svg"
 import pinIcons from '../assets/icons/pinIcons.svg'
+import addCircle from '../assets/icons/addCircle.svg'
 
 const ChatList = () => {
     return ( 
-    <section>
+    <section className="relative h-full">
         <form className="mb-6">
             <label htmlFor="chatSearch" className="relative">
                 <img src={searchIcon} alt="search bar icon" width={20} height={20} className="absolute left-1 top-0"/>
@@ -12,7 +13,8 @@ const ChatList = () => {
             </label>
         </form>
 
-        <ul className="flex flex-col gap-4">
+        <ul className="flex flex-col gap-4 overflow-y-scroll h-[80%] scroll-smooth">
+            
             <li className="flex w-full gap-2 hover:bg-gray-300 p-1 rounded-md">
                 <img src={userpfp} alt="user pfp" width={50} height={50} className="rounded-md"/>
                 <section className="flex justify-between w-full">
@@ -26,9 +28,8 @@ const ChatList = () => {
                     </article>
                 </section>
             </li>
-
-            
         </ul>
+        <button className="absolute bottom-0 right-2 hover:scale-110"><img src={addCircle} alt="add chat icon" height={35} width={35}/></button>
     </section>
     );
 }
